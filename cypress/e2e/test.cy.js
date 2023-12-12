42291714daa2225d161a396a70861859cc0965dc
Ghydaa Abu-zainh <ghydaa.abuzainh3@gmail.com>
2023-12-12 22:24:56 +0300

    hotels

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,37 +1,42 @@
 /// <reference types= "cypress" />
 
-describe('Aspire testcase', () => {
-    it.skip('check the currency', () => {
-        const expectedcurrency= 'SAR'
-        cy.visit('https://global.almosafer.com/en')
-        cy.get('.cta__saudi').click()
-        cy.get('[data-testid="Header__CurrencySelector"]').invoke('text').should('include',expectedcurrency)
-    });
+describe("Aspire testcase", () => {
+  it.skip("check the currency", () => {
+    const expectedcurrency = "SAR";
+    cy.visit("https://global.almosafer.com/en");
+    cy.get(".cta__saudi").click();
+    cy.get('[data-testid="Header__CurrencySelector"]')
+      .invoke("text")
+      .should("include", expectedcurrency);
+  });
 
-    // it('check the language', () => {
-    //     const attr='lang'
-    //     const expectedlang='en'
-        
-    //     cy.visit('https://global.almosafer.com/en')
-    //     cy.get('.cta__saudi').click()
-    //     cy.get('html').should('have.attr' , attr , expectedlang)
+  // it('check the language', () => {
+  //     const attr='lang'
+  //     const expectedlang='en'
 
+  //     cy.visit('https://global.almosafer.com/en')
+  //     cy.get('.cta__saudi').click()
+  //     cy.get('html').should('have.attr' , attr , expectedlang)
 
+  // });
 
-        
-    // });
+  it("check the language", () => {
+    let websites = [
+      "https://global.almosafer.com/en",
+      "https://global.almosafer.com/ar",
+    ];
 
-    it('check the language', () => {
+    let RandomIndex = Math.floor(Math.random() * websites.length);
+    cy.visit(websites[RandomIndex]);
+    cy.get(".cta__saudi").click();
+    cy.get('#uncontrolled-tab-example-tab-hotels').click()
 
-        let websites = ["https://global.almosafer.com/en" , "https://global.almosafer.com/ar"]
+    if (RandomIndex == 0 ){
+        cy.get('[data-testid="AutoCompleteInput"]').type('Dubai')
+    }
 
-        let RandomIndex = Math.floor(Math.random()*websites.length)
-
-       
-       cy.visit(websites[RandomIndex])
-       cy.get('.cta__saudi').click()
-       
-
-        
-    });
+    else if ( RandomIndex == 1) {
+        cy.get('[data-testid="AutoCompleteInput"]').type('دبي')
+    }
+  });
 });
